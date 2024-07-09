83c5a27dd310a4a10d6cd119ea09a800d9ce4032
Alex <sviraka@gmail.com>
2024-07-09 18:29:40 +0300

    Revert "12"
    This reverts commit 6748bc189cf5d299dc967870ec9c157d3a45b013.

---
+++ mainBot.py
@@ -0,0 +1,26 @@
+# 👉 🙏 👆 👇 😅 👋 🙌 ☺️ ❗ ️‼️ ✌️ 👌 ✊ 👨‍💻  🤖 😉  ☝️ ❤️ 💪 ✍️ 🎯  ⛔  ️✅ 📊📈🧮 -- 🗳️ 🏠 👷🏼
+#import json, time, random
+import time
+
+from aiogram.utils import executor
+from helper_init import dp, bot
+
+
+from handlers import client
+client.register_handlers(dp)
+
+
+
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+    while True:  # запускаем бесконечный цикл
+        try:  # пробуем наладить связь с сервером Telegram
+            bot.polling(none_stop=True)  # polling отправляет запросы на сервара
+        except Exception as e:  # если произойдет ошибка, то
+            time.sleep(3)  # подождать 3 секунды
+            print(e)  # и вывести logi ошибки в консоль компьютера
+
+#//////////////////////////////////////////////////////////////////////////////////////////    
